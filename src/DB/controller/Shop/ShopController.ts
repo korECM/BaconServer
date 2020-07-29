90b6dd3f58a03bb1960d8b9990e7d6af343fe1ce
JongwonYoun <jeffyoun@naver.com>
2020-07-29 22:38:17 +0900

    FEAT: ShopController 작성

---
+++ src/DB/controller/Shop/ShopController.ts
@@ -0,0 +1,37 @@
+import Shop, { ShopInterface, ShopSchemaInterface, ShopCategory } from '../../models/Shop';
+import { KeywordSchemaInterface } from '../../models/Keyword';
+
+export class ShopController {
+  constructor() {}
+
+  async findById(id: string): Promise<ShopSchemaInterface | null> {
+    return await Shop.findById(id);
+  }
+
+  async createShop(
+    name: string,
+    contact: string,
+    address: string,
+    image: string[],
+    keyword: KeywordSchemaInterface,
+    category: ShopCategory[],
+  ): Promise<ShopInterface | null> {
+    let shop = new Shop({
+      name,
+      contact,
+      address,
+      image,
+      category,
+      keyword,
+    });
+
+    try {
+      await shop.save();
+
+      return shop as ShopInterface;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+}
