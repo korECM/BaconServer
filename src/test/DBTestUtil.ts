5026e97ad2fe2f996e72d2ced8ffb4a78381e7be
JongwonYoun <jeffyoun@naver.com>
2020-07-31 22:21:49 +0900

    [FEAT]: DBTest를 위한 Util 파일 작성

---
+++ src/test/DBTestUtil.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+export function setupDB(databaseName: string) {
+  beforeAll(async () => {
+    const url = `mongodb://127.0.0.1/${databaseName}`;
+    await mongoose.connect(url, { useNewUrlParser: true });
+  });
+
+  // Cleans up database between each test
+  afterEach(async () => {
+    // await removeAllCollections();
+  });
+
+  // Disconnect Mongoose
+  afterAll(async () => {
+    await dropAllCollections();
+    await mongoose.connection.close();
+  });
+}
+
+export async function removeAllCollections() {
+  const collections = Object.keys(mongoose.connection.collections);
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName];
+    await collection.deleteMany({});
+  }
+}
+
+export async function dropAllCollections() {
+  const collections = Object.keys(mongoose.connection.collections);
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName];
+    try {
+      await collection.drop();
+    } catch (error) {
+      // This error happens when you try to drop a collection that's already dropped. Happens infrequently.
+      // Safe to ignore.
+      if (error.message === 'ns not found') return;
+
+      // This error happens when you use it.todo.
+      // Safe to ignore.
+      if (error.message.includes('a background operation is currently running')) return;
+
+      console.log(error.message);
+    }
+  }
+}
