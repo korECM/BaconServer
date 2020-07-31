013fb6e23c68f90b14e1c977e2d81f6d528e4a8f
JongwonYoun <jeffyoun@naver.com>
2020-07-31 22:25:03 +0900

    [FIX]: mongoDB Connect 할 때 UnifiedTopology 설정

--- src/test/DBTestUtil.ts
+++ src/test/DBTestUtil.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export function setupDB(databaseName: string) {
   beforeAll(async () => {
     const url = `mongodb://127.0.0.1/${databaseName}`;
-    await mongoose.connect(url, { useNewUrlParser: true });
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
   });
 
   // Cleans up database between each test
