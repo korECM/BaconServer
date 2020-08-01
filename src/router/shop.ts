a6b64d6b2a0d800c02555693f7f5c71cfe8c0ade
JongwonYoun <jeffyoun@naver.com>
2020-08-01 18:58:31 +0900

    [FEAT]: Shop Review 추가 Router 작성

--- src/router/shop.ts
+++ src/router/shop.ts
@@ -3,6 +3,8 @@ import Joi from 'joi';
 import { isInvalid } from './validate';
 import { ShopService, ShopOrder } from '../service/ShopService';
 import { Location } from '../DB/models/Shop';
+import { isValidObjectId } from 'mongoose';
+import { ReviewController } from '../DB/controller/Review/ReviewController';
 
 const router = express.Router();
 
@@ -16,6 +18,27 @@ router.get('/', async (req, res, next) => {
   let shops = await shopService.getShops({ category: category as any, location: location as any, order: order as any });
 
   res.status(200).json(shops);
+});
+
+router.post('/review/:shopId', async (req, res, next) => {
+  const shopId = req.params.shopId as string;
+  if (!shopId || shopId.length === 0) return res.status(404).send();
+  if (isValidObjectId(shopId) === false) return res.status(404).send();
+  const { score } = req.body;
+  if (!score || isNaN(Number(score))) return res.status(404).send();
+
+  if (!req.user) return res.status(401).send();
+
+  let reviewController = new ReviewController();
+  try {
+    let result = await reviewController.createReview(score, req.user._id, shopId);
+    if (result === null) return res.status(400).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
+
+  return res.status(201).send();
 });
 
 export default router;
