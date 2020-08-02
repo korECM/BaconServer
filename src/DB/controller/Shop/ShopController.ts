e25887a2a05ddac8d4d5c8ed6061034c70e33807
JongwonYoun <jeffyoun@naver.com>
2020-08-02 17:00:43 +0900

    [FEAT]: getShops의 reviews에 user Join하도록

--- src/DB/controller/Shop/ShopController.ts
+++ src/DB/controller/Shop/ShopController.ts
@@ -1,6 +1,7 @@
 import Shop, { ShopInterface, ShopSchemaInterface, ShopCategory, Location } from '../../models/Shop';
 import Keyword, { KeywordSchemaInterface } from '../../models/Keyword';
 import { ShopOrder } from '../../../service/ShopService';
+import User from '../../models/User';
 
 export class ShopController {
   constructor() {}
@@ -12,8 +13,10 @@ export class ShopController {
   async getShops(filter: any, order: ShopOrder, withOrder: boolean): Promise<ShopInterface[] | null> {
     // TODO: 정렬 방식 추가 필요
     if (withOrder) {
-      return await Shop.aggregate([
+      let shops = await Shop.aggregate([
+        // 해당 가게 찾은 후에
         { $match: filter },
+        // reviews에 Review Join
         {
           $lookup: {
             from: 'reviews',
@@ -22,10 +25,20 @@ export class ShopController {
             as: 'reviews',
           },
         },
+        // Review의 shop 안보이도록
+        {
+          $project: {
+            'reviews.shop': 0,
+          },
+        },
+        // Review 평균 구해서 scoreAverage 추가
         {
           $addFields: { scoreAverage: { $avg: '$reviews.score' } },
         },
       ]);
+      // reviews 안에 있는 userId Join
+      await User.populate(shops, { path: 'reviews.user', select: '_id name email' });
+      return shops;
     } else {
       return await Shop.find(filter);
     }
