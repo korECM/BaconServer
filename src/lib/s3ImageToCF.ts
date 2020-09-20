f8fe6c6d2ce692eaf1d63884eb1a1ef29f876c70
JongwonYoun <jeffyoun@naver.com>
2020-09-20 14:59:48 +0900

    FEAT: s3 이미지 cf로 변환 로직 추가

---
+++ src/lib/s3ImageToCF.ts
@@ -0,0 +1,24 @@
+import DB from '../DB';
+import { MenuInputInterface, ShopController } from '../DB/controller/Shop/ShopController';
+import dotenv from 'dotenv';
+import Shop, { FoodCategory, ShopCategory } from '../DB/models/Shop';
+import Image from '../DB/models/Image';
+dotenv.config();
+
+(async () => {
+  await DB.connect();
+  let images = await Image.find();
+  for (const image of images) {
+    console.log(image.imageLink);
+    image.imageLink = image.imageLink.replace('bacon-shop-origin.s3.ap-northeast-2.amazonaws.com', 'd3s32mx82uelsl.cloudfront.net');
+    await image.save();
+  }
+  let shops = await Shop.find();
+  for (const shop of shops) {
+    if (shop.mainImage.length > 0) {
+      shop.mainImage = shop.mainImage.replace('bacon-shop-origin.s3.ap-northeast-2.amazonaws.com', 'd3s32mx82uelsl.cloudfront.net');
+      await shop.save();
+    }
+  }
+  console.log('Image 변환 완료');
+})();
