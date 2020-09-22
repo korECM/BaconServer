a7f26a685430447103a6c1901513adc56b82bff1
JongwonYoun <jeffyoun@naver.com>
2020-09-22 18:35:07 +0900

    REFACT: image 변환 로직 효율적으로 수정

--- src/lib/s3ImageToCF.ts
+++ src/lib/s3ImageToCF.ts
@@ -9,13 +9,16 @@ dotenv.config();
   await DB.connect();
   let images = await Image.find();
   for (const image of images) {
-    console.log(image.imageLink);
-    image.imageLink = image.imageLink.replace('bacon-shop-origin.s3.ap-northeast-2.amazonaws.com', 'd3s32mx82uelsl.cloudfront.net');
-    await image.save();
+    if (image.imageLink.includes('bacon-shop-origin.s3.ap-northeast-2.amazonaws.com')) {
+      console.log(`${image.shopId} image 변환`);
+      image.imageLink = image.imageLink.replace('bacon-shop-origin.s3.ap-northeast-2.amazonaws.com', 'd3s32mx82uelsl.cloudfront.net');
+      await image.save();
+    }
   }
   let shops = await Shop.find();
   for (const shop of shops) {
-    if (shop.mainImage.length > 0) {
+    if (shop.mainImage.length > 0 && shop.mainImage.includes('bacon-shop-origin.s3.ap-northeast-2.amazonaws.com')) {
+      console.log(`${shop.name} mainImage 변환`);
       shop.mainImage = shop.mainImage.replace('bacon-shop-origin.s3.ap-northeast-2.amazonaws.com', 'd3s32mx82uelsl.cloudfront.net');
       await shop.save();
     }
