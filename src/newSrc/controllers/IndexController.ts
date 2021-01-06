864fb4b12e4e95b884f16f68100164aaa0c7bf28
JongwonYoun <jeffyoun@naver.com>
2021-01-06 21:16:37 +0900

    FIX: 필요없는 라우터 경로 삭제

--- src/newSrc/controllers/IndexController.ts
+++ src/newSrc/controllers/IndexController.ts
@@ -1,30 +1,21 @@
-import {Get, JsonController, Param, Res} from "routing-controllers";
+import {Get, HttpCode, JsonController, Res} from "routing-controllers";
 import {Response} from "express";
-import {logger} from "../utils/logger";
 import {OpenAPI} from "routing-controllers-openapi";
 
 @JsonController("")
 export class IndexController {
 
     @Get("")
+    @HttpCode(200)
     @OpenAPI({
         description: "기본 Index에 대한 API로 Hello를 반환한다",
         responses: {
-            '201': {
+            '200': {
                 description: "status 테스트를 위해 그냥 201를 반환한다"
             }
         },
     })
     public test(@Res() res: Response) {
-        logger.info("asdf");
-        return res.status(201).send("Hello");
-    }
-
-    @Get("/:id")
-    public idTest(@Param("id") id: string) {
-        if (id !== "1") {
-            throw new Error("id test : " +  id);
-        }
-        return id;
+        return res.status(200).send("Hello");
     }
 }
