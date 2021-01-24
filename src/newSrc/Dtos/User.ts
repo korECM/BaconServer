e3c3bcdc4b9af87523fd8a7c8301939c81beacd4
JongwonYoun <jeffyoun@naver.com>
2021-01-25 01:17:14 +0900

    feat: UserController.ts signInLocal 추가

--- src/newSrc/Dtos/User.ts
+++ src/newSrc/Dtos/User.ts
@@ -1,12 +1,45 @@
 import {AuthProvider, Gender, Role} from "../domains/User/User";
+import {IsBoolean, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsString, ValidateIf} from "class-validator";
 
 export class UserForSignInResponse {
-    constructor(public id: number, public name: string, public email: string | null, public gender: Gender, public role: Role, public snsNameSet: boolean) {
+    @IsNumber()
+    @IsNotEmpty()
+    public id: number;
+    @IsString()
+    @IsNotEmpty()
+    public name: string;
+    @ValidateIf(o => !o)
+    @IsString()
+    @IsNotEmpty()
+    public email: string | null;
+    public gender: Gender;
+    @IsNotEmpty()
+    @IsEnum(Role)
+    public role: Role;
+    @IsBoolean()
+    public snsNameSet: boolean;
+
+    constructor(id: number, name: string, email: string | null, gender: Gender, role: Role, snsNameSet: boolean) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.role = role;
+        this.snsNameSet = snsNameSet;
     }
 }
 
 export class UserForLocalSignInRequest {
-    constructor(public email: string, public password: string) {
+    @IsEmail()
+    @IsString()
+    public email: string;
+    @IsString()
+    @IsNotEmpty()
+    public password: string;
+
+    constructor(email: string, password: string) {
+        this.email = email;
+        this.password = password;
     }
 }
 
